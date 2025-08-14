8f68dbb376d119a46a26d3fe5d425ff56ebbeb42
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-14 13:47:00 +0530

    updated

---
+++ 03formHandle/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+let app = express()
+let PORT = 3535
+
+app.get('/', (req, res) => {
+    res.send("<h1>Test Page</h1>")
+})
+
+app.listen(PORT, ()=> {
+    return(`Server is running at ${PORT}`)
+})
