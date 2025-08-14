8fb640675a58d61bd3a6b10e83525eb7b3d7324b
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-14 08:52:10 +0530

    updated

---
+++ 02server/index.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const { default: home, service, course, contact, login, about } = require('./pages');
+const app = express();
+
+
+app.get("/", (request, response) => {
+    response.send(home())
+})
+app.get("/service", (request, response) => {
+    response.send(service())
+})
+app.get("/course", (request, response) => {
+    response.send(course())
+})
+app.get("/contact", (request, response) => {
+    response.send(contact())
+})
+
+app.get("/login", (request, response) => {
+    response.send(login())
+})
+
+app.get("/about", (request, response) => {
+    response.send(about())
+} )
+
+const PORT = 3939
+app.listen(PORT, ()=>{
+    console.log(`Server is Running at ${PORT}`)
+})
---
+++ 02server/pages.js
@@ -0,0 +1,22 @@
+
+export default function home () {
+    return("<h1> This is Landing Page</h1>")
+}
+
+
+export function about () {
+    return ("<h1> This is About Page</h1>")
+}
+export function service () {
+    return ("<h1> This is Service Page</h1>")
+}
+export function course () {
+    return ("<h1> This is course Page</h1>")
+}
+export function contact () {
+    return ("<h1> This is Contact Page</h1>")
+}
+export function login () {
+    return ("<h1> This is Login Page</h1>")
+}
+
