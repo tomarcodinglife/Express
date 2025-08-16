69125294341c591627eb7a036c78376dd5697864
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-16 07:15:55 +0530

    updated

--- 06websiteCreatUsingImport/index.js
+++ 06websiteCreatUsingImport/index.js
@@ -1,13 +1,32 @@
 import express, { request, response } from 'express'
+import path, { resolve } from 'path'
 
 let app = express()
 let PORT = 3436
 
 
+
 app.get("/", (request, response) => {
-    response.send("<h1>Server Running</h1>")
+    const absolutePath = path.resolve('views/home.html')
+    console.log(absolutePath)
+    response.sendFile(absolutePath)
+})
+
+app.get('/login', (request, response) => {
+    const abPath = path.resolve('views/login.html')
+    response.sendFile(abPath)
+})
+
+app.get('/about', (request, response) => {
+    const absolutePath = path.resolve('views/about.html')
+    response.sendFile(absolutePath)
+})
+
+app.get('/contact', (request, response) => {
+    const absolutePath = path.resolve('views/contact.html')
+    response.sendFile(absolutePath)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running at ${PORT} `)
+    console.log(`Server running at ${PORT}`)
 })
