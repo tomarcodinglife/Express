f6a696e2d0d7965f870c08371cbabffabfd4d572
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-25 09:25:02 +0530

    Form Input Handle file updated

--- 11formDataDisplay/index.js
+++ 11formDataDisplay/index.js
@@ -1,8 +1,25 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 const PORT = 5952
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (request, response) => {
+    response.render('home', {
+        title : 'Home Page',
+        heading : 'Welcome to Home Page'
+    })
+})
+
+app.get('/addUser', (request, response) => {
+    response.render('addUser')
+})
+
+app.post('/submitUsers', (request, response) => {
+    response.render('submitUsers', request.body)
+})
 
 app.listen(PORT, () => {
     console.log(`Server Running on ${PORT}`)
