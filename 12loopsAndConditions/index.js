077f0440a8b8abdeab487077f06198f277022326
tomarcodinglife <tomarcodinglife@gmail.com>
2025-08-31 12:38:57 +0530

    updated

--- 12loopsAndConditions/index.js
+++ 12loopsAndConditions/index.js
@@ -5,9 +5,15 @@ const PORT = 3631
 
 
 app.get('/', (request, response)=> {
-    response.render()
+    response.render('home')
 })
 
+app.get('/addUser', (request, response)=> {
+    response.render('addUser')
+})
+app.get('/submitUsers', (request, response)=> {
+    response.render('submitUsers')
+})
 
 
 app.listen(PORT, () => {
