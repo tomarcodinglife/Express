bc00daedd880cd805badf7e3b89a328f1558e787
tomarcodinglife <tomarcodinglife@gmail.com>
2025-09-04 07:54:12 +0530

    updated

---
+++ 16dbConnect/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import { MongoClient } from 'mongodb'
+
+const app = express()
+const PORT = 3948
+
+const dbName = "mydb"
+const mongoDBurl = "mongodb://localhost:27017"
+
+
+const client = new MongoClient(mongoDBurl)
+
+async function dbConnection(){
+    await client.connect()
+    console.log("✅ Connected to MongoDB")
+
+    const db = client.db(dbName)
+    const collection = db.collection('users');
+
+    const result = await collection.find().toArray()
+    console.log(result)
+}
+
+dbConnection()
+
+app.get('/', (request, response) => {
+    response.send('Home Page')
+})
+
+
+app.listen(PORT, (request, response) => {
+    console.log(`Server Running on ${PORT}`);
+})
