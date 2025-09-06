6b9b4d5647980dfd48d1b54c1a5b258d61284eb7
tomarcodinglife <tomarcodinglife@gmail.com>
2025-09-06 08:10:15 +0530

    updated

--- 19_REST_API_update_with_form_in_NodeJS_MongoDB/index.js
+++ 19_REST_API_update_with_form_in_NodeJS_MongoDB/index.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express'
+import express from 'express'
 import { MongoClient } from 'mongodb'
 
 const app = express()
@@ -10,23 +10,35 @@ const mongodbURL = 'mongodb://localhost:27017'
 const client = new MongoClient(mongodbURL)
 
 app.set('view engine', 'ejs')
+
+// Middleware for form & JSON parsing
+app.use(express.urlencoded ({extended:true}))
+app.use(express.json())
+
 client.connect().then((connection)=>{
     const db = connection.db(dbName)
+    const collection = db.collection('users')
 
     app.get('/', async (request, response)=>{
-    const collection = db.collection('users')
     const users = await db.collection('users').find().toArray()
     response.render('home', {users})
     })
 
     app.get('/api', async (request, response)=>{
-    const collection = db.collection('users')
     const users = await db.collection('users').find().toArray()
     response.send(users)
     })
 
     app.get('/form', (request, response) => {
     response.render('form')
+    })
+
+    app.post('/add_student', async (request, response) => {
+        // console.log(request.body);
+
+        const result = await collection.insertOne(request.body)
+        console.log(result)
+        response.send('Data Saved')
     })
 
 })
