834db0745e40ac921e855f10d6d195faddc50820
tomarcodinglife <tomarcodinglife@gmail.com>
2025-09-12 09:15:39 +0530

    update

--- 21deleteMethodRestAPI_with_NodeJS_Mongodb/index.js
+++ 21deleteMethodRestAPI_with_NodeJS_Mongodb/index.js
@@ -9,21 +9,57 @@ const mongodbURL = 'mongodb://localhost:27017';
 const client = new MongoClient(mongodbURL);
 
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 client.connect().then((connsection)=>{
     const db = connsection.db(dbName)
 
-    app.get('/', (request, response)=>{
+    app.get('/', async (request, response)=>{
         const collection = db.collection('users')
-        const users = collection.find().toArray();
+        const users = await collection.find().toArray();
         response.render('home', {users})
     })
 
-    app.get('/api', (request, response)=>{
+    app.get('/api', async (request, response)=>{
         const collection = db.collection('users')
-        const users = collection.find().toArray();
+        const users = await collection.find().toArray();
         response.send(users)
+    })
+
+    app.get('/form', (request, response)=>{
+        response.render('form')
+    })
+    
+    app.post('/add-user', async (request, response)=>{
+
+        if(!request.body.name || !request.body.email || !request.body.age || !request.body.city){
+            response.status(400).send({message:'Incomplete details'})
+            return;
+        }else{
+            const collection = db.collection('users')
+            const result = await collection.insertOne(request.body)
+            console.log(result);
+            response.send('Data Saved')
+        }
+        console.log(request.body);
+    })
+
+    app.delete('/delete-user/:id', (request, response)=>{
+        const id = request.params.id;
+        const collection = db.collection('users')
+        const result = collection.deleteOne({_id: new ObjectId(request.params.id)})
+        if(result){
+            response.send({message: 'User Deleted', sucess: true})
+        }else{
+            response.status(400).send({message: 'User Not Found', sucess: false})
+        }
+        console.log(id);
+    })
+
+    app.listen(port, ()=>{
+        console.log(`Server is running on http://localhost:${port}`);
     })
 
 })
