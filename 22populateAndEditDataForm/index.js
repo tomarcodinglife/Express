0cc5def79b439a1feaee952a55ebb90a3ebac7dc
tomarcodinglife <tomarcodinglife@gmail.com>
2025-09-13 20:00:58 +0530

    updated

---
+++ 22populateAndEditDataForm/index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+
+const app = express();
+const port = 3636;
+
+const dbName = 'mydb';
+const mongodbURL = 'mongodb://localhost:27017';
+const client = new MongoClient(mongodbURL);
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+client.connect().then((connsection)=>{
+    const db = connsection.db(dbName)
+
+    app.get('/', async (request, response)=>{
+        const collection = db.collection('users')
+        const users = await collection.find().toArray();
+        response.render('home', {users})
+    })
+
+    app.get('/add-data', (request, response)=>{
+        response.render('add-data')
+    })
+
+    app.post('/submit-data', async (request, response)=>{
+
+        if(!request.body.name || !request.body.email || !request.body.age || !request.body.city){
+            response.status(400).send({message:'Incomplete details'})
+            return;
+        }else{
+            const collection = db.collection('users')
+            const result = await collection.insertOne(request.body)
+            console.log(result);
+            response.send('Data Saved')
+        }
+        console.log(request.body);
+    })
+
+    app.post('/edit-user/:id', async (request, response)=>{
+        const id = request.params.id;
+        const collection = db.collection('users')
+        const result = await collection.updateOne(
+            {_id: new ObjectId(id)},
+            {$set: request.body}
+        )
+        if(result){
+            response.send({message: 'User Updated', sucess: true})
+        }else{
+            response.status(400).send({message: 'User Not Found', sucess: false})
+        }
+        console.log(id);
+    })
+    
+
+    app.listen(port, ()=>{
+        console.log(`Server started at http://localhost:${port}`);
+    })
+
+})
