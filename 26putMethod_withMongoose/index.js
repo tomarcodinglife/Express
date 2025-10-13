015e12d3a4656f536e1d8d0b2a6a60017d3e46ce
Sujit Tomar <tomarcodinglife@gmail.com>
2025-10-13 19:24:00 +0530

    updated

---
+++ 26putMethod_withMongoose/index.js
@@ -0,0 +1,9 @@
+import { response } from "express";
+import express from express
+
+const app = express();
+const port = 3526;
+
+app.get("/", (request, response)=>{
+    response.send("Hello My Dear Friend")
+})
